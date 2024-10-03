f0d990b084e1e9845c26d43023c9658bd4030c18
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2024-10-03 15:20:17 +0900

    [add] しりとりテンプレートを追加

--- server.deno.js
+++ server.deno.js
@@ -1,17 +1,53 @@
-import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
+import { serveDir } from "https://deno.land/std@0.223.0/http/file_server.ts";
 
-Deno.serve(async (req) => {
-  const pathname = new URL(req.url).pathname;
-  console.log(pathname);
+let previousWord = "しりとり";
 
-  if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
+Deno.serve(async (request) => {
+  // パス名を取得する
+  // http://localhost:8000/hoge に接続した場合"/hoge"が取得できる
+  const pathname = new URL(request.url).pathname;
+  console.log(`pathname: ${pathname}`);
+
+  if (request.method === "GET" && pathname === "/shiritori") {
+    return new Response(previousWord);
   }
 
-  return serveDir(req, {
-    fsRoot: "public",
+  if (request.method === "POST" && pathname === "/shiritori") {
+    // リクエストのペイロードを取得
+    const requestJson = await request.json();
+    // JSONの中からnextWordを取得
+    const nextWord = requestJson["nextWord"];
+
+    // previousWordの末尾とnextWordの先頭が同一か確認
+    if (previousWord.slice(-1) === nextWord.slice(0, 1)) {
+      // 同一であれば、previousWordを更新
+      previousWord = nextWord;
+    } else {
+      return new Response(
+        JSON.stringify({
+          errorMessage: "前の単語に続いていません",
+          errorCode: "10001",
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json; charset=utf-8" },
+        }
+      );
+    }
+
+    // 現在の単語を返す
+    return new Response(previousWord);
+  }
+
+  // ./public以下のファイルを公開
+  return serveDir(request, {
+    /*
+             - fsRoot: 公開するフォルダを指定
+             - urlRoot: フォルダを展開するURLを指定。今回はlocalhost:8000/に直に展開する
+             - enableCors: CORSの設定を付加するか
+             */
+    fsRoot: "./public/",
     urlRoot: "",
-    showDirListing: true,
     enableCors: true,
   });
 });
